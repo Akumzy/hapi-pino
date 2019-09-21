c92679bc40bbaf09802b4f1ec0db0c03bcf9a565
Akuma Isaac Akuma <akumaisaacakuma@gmail.com>
2019-09-21 16:55:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -117,7 +117,7 @@ async function register (server, options) {
     }
 
     const info = request.info
-    request.logger.info(
+    (request.logger?reguest.logger:logger).info(
       {
         payload: options.logPayload ? request.payload : undefined,
         tags: options.logRouteTags ? request.route.settings.tags : undefined,
